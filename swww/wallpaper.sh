e64ed6a84958ecba4aa6af6e093b146630824e95
Fronkuri <jpauloalmeida28@gmail.com>
2025-08-21 17:22:00 -0300

    Update wallpaper.sh

--- swww/wallpaper.sh
+++ swww/wallpaper.sh
@@ -3,7 +3,6 @@
 cd ~/.config/swww/hyprWallpapers/
 ranger --choosefile=wallpaper_dir.txt
 
-background_path = $(cat wallpaper_dir.txt)
 cp $(cat wallpaper_dir.txt) ~/.config/swww/wallpaper.png
 swww img $(<wallpaper_dir.txt)
 rm wallpaper_dir.txt
