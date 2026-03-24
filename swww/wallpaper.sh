c2ea32d79114a1dc316b3f9e9fed7e576e584624
Fronkuri <jpauloalmeida28@gmail.com>
2026-03-24 18:54:20 -0300

    Delete swww directory

--- swww/wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/.config/swww/hyprWallpapers/
-ranger --choosefile=wallpaper_dir.txt
-
-cp $(cat wallpaper_dir.txt) ~/.config/swww/wallpaper.png
-swww img $(<wallpaper_dir.txt)
-rm wallpaper_dir.txt
